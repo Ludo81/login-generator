900df2ca93ee905c9ed75fd332b521dd1d1443cd
Ludo81 <ludovic.tournier@hotmail.fr>
2019-11-20 07:51:34 +0100

    Ajout des tests

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -1,42 +1,48 @@
 package geco;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
 public class LoginServiceTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(LoginService.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+
+    private  LoginService ls;
+    List<String> resul = new ArrayList<String>();
+    List<String> resulV = new ArrayList<String>();
 
     @Before
     public void setUp() throws Exception {
+
+        String[] logins = {"l1"};
+        resul.add("l1");
+        ls = new LoginService(logins);
+
     }
 
     @Test
     public void loginExists() {
+        assertTrue(ls.loginExists("l1"));
+        assertFalse(ls.loginExists("l2"));
     }
 
     @Test
     public void addLogin() {
+        ls.addLogin("l2");
     }
 
     @Test
     public void findAllLoginsStartingWith() {
+        assertEquals(ls.findAllLoginsStartingWith("l"), resul);
+        assertEquals(ls.findAllLoginsStartingWith("e"), resulV);
     }
 
     @Test
     public void findAllLogins() {
+        assertEquals(ls.findAllLogins(), resul);
     }
 }
